4b4d4dbf9d5e4e63c37d1a96c64931c9b3e80844
Omochice <mochice.mls.ntl@gmail.com>
2023-03-06 13:14:25 +0900

    :muscle:
    - add type annotation
    - apply formatter

--- src/helloWorld.ts
+++ src/helloWorld.ts
@@ -1,21 +1,22 @@
 export class HelloWorld extends HTMLElement {
-  name: string
+  name: string;
+
   constructor() {
-    super()
-    this.name = "world"
+    super();
+    this.name = "world";
   }
 
-  attributeChangedCallback(prop: string, prev: string, next: string) {
-    if (prev === next) return
-    if (!Object.hasOwn(this, prop)) return
-    this[prop] = next
+  attributeChangedCallback(prop: string, prev: string, next: string): void {
+    if (prev === next) return;
+    if (!Object.hasOwn(this, prop)) return;
+    this[prop] = next;
   }
 
-  static get observedAttributes() {
-    return ["name"]
+  static get observedAttributes(): string[] {
+    return ["name"];
   }
 
-  connectedCallback() {
-    this.textContent = `Hello ${this.name}!`
+  connectedCallback(): void {
+    this.textContent = `Hello ${this.name}!`;
   }
 }
